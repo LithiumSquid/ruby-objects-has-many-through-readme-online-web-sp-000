c8b5b407618ec656d7fe44c3976ae822d31e83e8
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-11 00:57:21 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
+  attr_accessor :meal, :waiter
+  attr_reader :name, :age
+  
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.customer.all
+end
+  
+  def self.waiters
+    waiters.all
+  end
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
+  attr_accessor :customer, :meal
+  attr_reader :name, :experience
+  
+  @@all = []
+
+def initialize(name, experience)
+  @name = name
+  @experience = experience
+  @@all << self
+end
+
+def self.all
+  @@all
+end
 
 end
